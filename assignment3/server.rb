83ef4ebb0176b8e0a081beb930415178bd86ee63
gusmattos1 <gusmattos@gmail.com>
2018-04-08 13:55:49 -0400

    assignment 3 ongoing

--- assignment3/server.rb
+++ assignment3/server.rb
@@ -6,3 +6,24 @@ require 'sinatra'
 get '/home' do
   erb :index
 end
+
+get '/portifolio' do
+  erb :portifolio
+end
+
+get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
+  erb :about_me
+end
+
+get '/favourites' do
+  @favourites = [
+    "http://www.facebook.com", "http://www.instagram.com", "http://www.google.com", "http://www.g1.com"
+  ]
+  erb :favourites
+end
+
+get '/' do
+  redirect to('/home')
+end

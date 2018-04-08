f7e94e9d05d5270d0ee4b7826ede4b5376684ea1
gusmattos1 <gusmattos@gmail.com>
2018-04-08 13:03:49 -0400

    assignment 2 done

--- assignment2/North-Pole-Writing-Machine/naughty_letter_writer.rb
+++ assignment2/North-Pole-Writing-Machine/naughty_letter_writer.rb
@@ -5,14 +5,25 @@ naughty_letter = File.read('templates/naughty_sample_template.txt.erb')
 
 kids_data.each_line do |kid|
 
-  kid_data_array = kid.split(' ')
-  action_array = kid.split (" | ")
+  kid_data_array = kid.split(" ")
+  action_array = kid.split(" |  ")
+  action = action_array[1]
 
   name     = kid_data_array[0]
   behavior = kid_data_array[1]
   toys     = kid_data_array[2..7]
   sample_toy = toys.sample
-  action = action_array[1]
+  while sample_toy == "Kaleidoscope"
+    sample_toy = toys.sample
+  end
+
+  other_toys = []
+
+  toys.each do |toy|
+    if toy != sample_toy
+      other_toys.push(toy)
+    end
+  end
 
   next unless behavior == 'naughty'
 
